4bcf30bd3fd995fd11048245ef9be3968fe28faa
李腾飞 <125561585@qq.com>
2021-03-02 08:51:34 +0800

    add array index knowledge

--- hello.go
+++ hello.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
 func main() {
-	pointer_test()
-	os.Exit(1)
+	array_test()
+	array_point()
 	fmt.Println("Please visit http://127.0.0.1:12345/")
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		s := fmt.Sprintf("你好，世界! -- Time:%s", time.Now().String())
@@ -19,5 +18,26 @@ func main() {
 	})
 	if err := http.ListenAndServe(":12345", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+//数组相关练习
+func array_test() {
+	var a [3]int
+	var b = [...]int{1, 2, 3}
+	var c = [...]int{2: 3, 1: 2}
+	var d = [...]int{1, 2, 4: 5, 6}
+	fmt.Println(a, b, c, d)
+}
+
+func array_point() {
+	//数组指针
+	var a = [...]int{1, 2, 3}
+	var b = &a
+	fmt.Println(a[0], a[1])
+	fmt.Println(b[0], b[1])
+
+	for i, v := range b {
+		fmt.Println(i, v)
 	}
 }
